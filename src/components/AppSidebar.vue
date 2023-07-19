99bfb81536031b574f4e2cd026eeecbbd09f3814
Egor Pobylets <egor.pobylets@cogniteq.com>
2023-07-19 08:28:32 +0200

    feat: add private layout

---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>Sidebar</nav>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title?: string
+}>()
+</script>
+
+<style scoped>
+  nav {
+    padding: 0 16px;
+    height: calc(100vh - 64px);
+    width: 240px;
+    background-color: var(--vt-c-white-back);
+  }
+</style>
--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,6 +1,13 @@
 export const ROUTES = {
+  home: '/',
   login: '/login',
   registration: '/registration',
+}
+
+export enum RouteNames {
+  home = 'home',
+  login = 'login',
+  registration = 'registration',
 }
 
 export const LOCAL_STORAGE_ITEMS = {
