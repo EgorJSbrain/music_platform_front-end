a8dc1a1fb29829f67d91e03ba00746be0ff4465b
Egor Pobylets <egor.pobylets@cogniteq.com>
2023-07-28 08:07:10 +0200

    feat: improving the track modal

---
+++ src/components/modals/AppTrackModal.vue
@@ -0,0 +1,56 @@
+<template>
+  <Modal title="Add track" :closeModal="toggleModalVisible" :is-visible="isModalVisible">
+    <div class="content">
+      <div class="upload-wrapper">
+        <UploadFile :type="UploadFileType.image" accept="image/*" :uploadFile="uploadImage" title="Upload image" />
+        <UploadFile :type="UploadFileType.audio" accept="audio/*" :uploadFile="uploadTrack" title="Upload track"/>
+      </div>
+      <Input placeholder="Track name" />
+      <Input placeholder="Author" />
+      <textarea placeholder="12" />
+    </div>
+  </Modal>
+</template>
+
+<script setup lang="ts">
+  import Modal from '@/components/AppModal.vue'
+  import Input from '@/components/AppInput.vue'
+  import { ButtonVariants, UploadFileType } from '@/constants/global';
+  import UploadFile from '@/components/AppUploadFile.vue';
+
+const props = defineProps<{
+  title?: string
+  isModalVisible: boolean
+  toggleModalVisible: () => void
+}>()
+
+const uploadImage = (file: File) => {
+  console.log("🚀 ~ file: StorageView.vue:28 ~ uploadImage ~ file:", file)
+}
+
+const uploadTrack = (file: File) => {
+  console.log("🚀 ~ file: StorageView.vue:28 ~ uploadImage ~ track:", file)
+}
+
+const handleApply = () => {
+  props.toggleModalVisible()
+}
+
+const handleClose = () => {
+  props.toggleModalVisible()
+}
+
+</script>
+
+<style scoped>
+.upload-wrapper {
+  display: flex;
+  justify-content: space-between;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+}
+</style>
--- src/views/StorageView.vue
+++ src/views/StorageView.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="panel"><Button :variant="ButtonVariants.contained" title="Add track" :click="toggleModalVisible"><IconMenu /></Button></div>
+  <div class="panel"><Button :variant="ButtonVariants.contained" title="Add track" :click="toggleModalVisible">
+      <IconMenu />
+    </Button></div>
   <div>!!!</div>
 
-  <Modal title="Add track" :closeModal="toggleModalVisible" :is-visible="isModalVisible">
-    <div class="upload-wrapper">
-      <AppUploadFile accept="image/*" :uploadFile="uploadImage" title="Upload image" />
-      <AppUploadFile accept="audio/*" :uploadFile="uploadTrack" title="Upload track"/>
-    </div>
-  </Modal>
+  <TrackModal
+    :toggleModalVisible="toggleModalVisible"
+    :isModalVisible="isModalVisible"
+  />
 </template>
 
 <script setup lang="ts">
   import { ref } from 'vue';
   import Button from '@/components/AppButton.vue'
   import IconMenu from '@/components/icons/IconMenu.vue';
-  import Modal from '@/components/AppModal.vue'
-  import { ButtonVariants } from '@/constants/global';
+  import TrackModal from '@/components/modals/AppTrackModal.vue'
+  import { ButtonVariants, UploadFileType } from '@/constants/global';
   import AppUploadFile from '@/components/AppUploadFile.vue';
 
   const isModalVisible = ref(false)
@@ -24,24 +24,11 @@
     isModalVisible.value = !isModalVisible.value
   }
 
-  const uploadImage = (file: File) => {
-    console.log("🚀 ~ file: StorageView.vue:28 ~ uploadImage ~ file:", file)
-  }
-
-  const uploadTrack = (file: File) => {
-    console.log("🚀 ~ file: StorageView.vue:28 ~ uploadImage ~ track:", file)
-  }
-
 </script>
 
 <style>
   .panel {
     display: flex;
     justify-content: end;
-  }
-
-  .upload-wrapper {
-    display: flex;
-    justify-content: space-between;
   }
 </style>
